df88f6233a13f53298370675ab656c8e036fc0ea
Raphael Ricard <raphael.ricard@epitech.eu>
2015-01-24 17:52:44 +0100

    uugh dead

--- Assets/Scripts/GameComponents/killMob.cs
+++ Assets/Scripts/GameComponents/killMob.cs
@@ -3,17 +3,27 @@ using System.Collections;
 
 public class            killMob : MonoBehaviour 
 {
-    void OnCollisionEnter2D(Collision2D col)
+  void OnCollisionEnter2D(Collision2D col)
+  {
+    if (col.gameObject.tag == "Player")
     {
-        if (col.gameObject.tag == "Player")
-        {
-            if (transform.position.y < col.gameObject.transform.position.y)
-            {
-                col.gameObject.GetComponent<CharacterController>().startJump();
-                Destroy(gameObject);
-            }
-            //else 
-            //    kill Player
-        }
+      if (transform.position.y < col.gameObject.transform.position.y)
+      {
+        col.gameObject.GetComponent<CharacterController>().startJump();
+        Destroy(gameObject);
+      }
+      else
+      {
+        col.gameObject.GetComponent<Animator>().Play("death");
+        col.gameObject.GetComponent<CharacterController>().dead();
+        StartCoroutine("loadDeath");
+      }
     }
+  }
+
+  IEnumerator loadDeath()
+  {
+    yield return new WaitForSeconds(2);
+    Application.LoadLevel(Application.loadedLevelName);
+  }
 }
