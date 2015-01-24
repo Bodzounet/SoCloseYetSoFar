9aed706cb3116481c051016faab2b9917cfa6be0
boulot_j <boulot_j@epitech.eu>
2015-01-24 12:53:59 +0100

    some other scripts

---
+++ Assets/Scripts/GameComponents/JumpingMob.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class            JumpingMob : MonoBehaviour
+{
+    public float        _jumpingSpeed;
+    private bool        _isJumping = false;
+    private bool        _unique = true;    
+
+	void Update () 
+    {
+        if (!_isJumping)
+        {
+            _isJumping = true;
+            _unique = true;
+            rigidbody2D.velocity = new Vector2(0, _jumpingSpeed);
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground" && _unique)
+        {
+            _unique = false;
+            StartCoroutine("rejump");
+        }
+    }
+
+    IEnumerator rejump()
+    {
+        yield return new WaitForSeconds(3);
+        _isJumping = false;
+    }
+}
---
+++ Assets/Scripts/GameComponents/killMob.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class            killMob : MonoBehaviour 
+{
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            if (transform.position.y < col.gameObject.transform.position.y)
+            {
+                col.gameObject.GetComponent<CharacterController>().startJump();
+                Destroy(gameObject);
+            }
+            //else 
+            //    kill Player
+        }
+    }
+}
