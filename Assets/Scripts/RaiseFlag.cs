4afd76c0836019ad195f8f2fd9e445e882101420
SmoothyD <damien.blanc@epitech.eu>
2015-01-25 00:28:46 +0100

    lvl1

---
+++ Assets/Scripts/RaiseFlag.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaiseFlag : MonoBehaviour {
+
+    IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSeconds(2);
+        Application.LoadLevel("lvl2");
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if (col.tag == "Player")
+        {
+            col.GetComponent<Animator>().Play("wtf");
+            GetComponent<Animator>().Play("raise_flag");
+        }
+    }
+}
