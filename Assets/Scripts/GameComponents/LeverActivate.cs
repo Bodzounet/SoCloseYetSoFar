b2e06fb6049f0175fd2cb5db28556a647fa37402
Raphael Ricard <raphael.ricard@epitech.eu>
2015-01-24 23:06:24 +0100

    lever state

--- Assets/Scripts/GameComponents/LeverActivate.cs
+++ Assets/Scripts/GameComponents/LeverActivate.cs
@@ -10,10 +10,23 @@ public class LeverActivate : MonoBehaviour
     private bool          _isTriggered;
     private Animator      _anim;
 
+  void  changeState()
+    {
+      _isTriggered = true;
+      _anim.SetBool("isActive", !_anim.GetBool("isActive"));
+      for (int i = 0; i < affectedBlock.Length; i++)
+      {
+        affectedBlock[i].SetActive(!affectedBlock[i].activeSelf);
+      }
+      StartCoroutine("reActivate");
+    }
+
     void Start()
     {
         _isTriggered = false;
         _anim = GetComponent<Animator>();
+        if (startupState)
+          changeState();
     }
 
     IEnumerator reActivate()
@@ -25,14 +38,6 @@ public class LeverActivate : MonoBehaviour
     void OnTriggerStay2D(Collider2D col)
     {
         if (_isTriggered == false && col.gameObject.tag == "Player" && Input.GetButton("Fire1"))
-        {
-            _isTriggered = true;
-            _anim.SetBool("isActive", _anim.GetBool("isActive"));
-            for (int i = 0; i < affectedBlock.Length; i++)
-            {
-                affectedBlock[i].SetActive(!affectedBlock[i].activeSelf);
-            }
-            StartCoroutine("reActivate");
-        }
+          changeState();
     }
 }
