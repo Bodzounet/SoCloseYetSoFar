bd2a4252eb701fb492efa1b426ff446547389236
boulot_j <boulot_j@epitech.eu>
2015-01-25 13:37:13 +0100

    tuto

---
+++ Assets/Scripts/GameComponents/endTuto.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class endTuto : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D col) 
+    {
+        StartCoroutine("endLvl");
+    }
+
+    IEnumerator endLvl()
+    {
+        yield return new WaitForSeconds(2);
+        Application.LoadLevel("Lvl1");
+    }
+}
