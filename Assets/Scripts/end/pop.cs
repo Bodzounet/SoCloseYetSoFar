a8c8745797b0a2c73c5b2696b668f624f21bb789
boulot_j <boulot_j@epitech.eu>
2015-01-25 13:48:53 +0100

    end

---
+++ Assets/Scripts/end/pop.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pop : MonoBehaviour {
+
+    TextMesh tm;
+    float incr = 0;
+
+	void Start () 
+    {
+        tm = GetComponent<TextMesh>();
+	}
+	
+	void Update () 
+    {
+        tm.color = Color.Lerp(Color.black, Color.red, incr);
+        incr += Time.deltaTime / 5;
+        if (incr > 1.3)
+            Application.LoadLevel("menu");
+	}
+
+}
