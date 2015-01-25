2214410a7a9027309c73c147cb3c032c2413d6a7
boulot_j <boulot_j@epitech.eu>
2015-01-25 03:46:09 +0100

    menu

---
+++ Assets/Scripts/menu/Play.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Play : MonoBehaviour {
+
+    Color from = Color.white;
+    Color to = Color.red;
+
+    TextMesh me;
+    private float timer;
+
+    bool kikoo = false;
+
+	// Use this for initialization
+	void Start () 
+    {
+        me = GetComponent<TextMesh>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (kikoo)
+            me.color = Color.Lerp(from, to, timer);
+        timer += Time.deltaTime;
+        if (timer > 1)
+            kikoo = false;
+	}
+
+    void OnMouseEnter()
+    {
+        kikoo = true;
+        from = me.color;
+        to = Color.red;
+        timer = 0;
+    }
+
+    void OnMouseExit()
+    {
+        kikoo = true;
+        from = me.color;
+        to = Color.white;
+        timer = 0;
+    }
+}
---
+++ Assets/Scripts/menu/returnToMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class returnToMenu : MonoBehaviour {
+	
+	void Update () 
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+            Application.LoadLevel("Menu");
+	}
+}
